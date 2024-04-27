3789e03cea86808e16253b40ccb90c5db32bc15c
MikhailVolvach <mihailvolvach@gmail.com>
2024-04-27 18:28:27 +0300

    in progress

--- src/components/chat/ChatMessageList.tsx
+++ src/components/chat/ChatMessageList.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 
+import Slide from '@mui/material/Slide';
+
 import { User } from '../../api/generated';
 import { useCurrentUser } from '../../state/current-user/slice';
 import { Message } from '../../types/Message';
